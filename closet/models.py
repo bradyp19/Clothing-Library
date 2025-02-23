fd1438d96b89ad4f90cae58c55e1f72bdf2bbb25
jadenChristy <vra8qu@virginia.edu>
2025-02-23 13:43:57 -0500

    added login and logout screens

--- closet/models.py
+++ closet/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class Item(models.Model):
     item_name = models.CharField(max_length=60)
@@ -80,3 +81,14 @@ class Shoes(Item):
         ("13", "13"),
     ]
     size = models.CharField(max_length=4, choices=SIZE_CHOICES)
+
+# classes for base user, patron, and librarian.
+
+#class BaseUser(AbstractUser):
+#    email = models.EmailField(max_length=20, unique=True)
+#    name = models.CharField(max_length=120)
+
+#class Patron(BaseUser):
+
+#class Librarian(BaseUser):
+
