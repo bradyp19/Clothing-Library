a0809e57d582cde82c861acbad51be264c6851c7
Zhiyuan Chang <vgs3qt@virginia.edu>
2025-02-23 02:09:34 -0500

    Adding urls for account login part

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("closet/", include("closet.urls")),
     path('admin/', admin.site.urls),
+    path("closet/", include("closet.urls")),
+    path('accounts/', include('allauth.urls')),
 ]
