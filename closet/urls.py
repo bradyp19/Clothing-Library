4054f0e85b60e5d3505a6cc3b13f41021bca2e3e
jadenChristy <vra8qu@virginia.edu>
2025-02-23 15:08:30 -0500

    fixed url path in closet/urls.py for dashboard

--- closet/urls.py
+++ closet/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "closet"
 urlpatterns = [
     #path("", views.LoginView.as_view(), name="login"),
-    path("dashboard", views.dashboard, name="dashboard"),  # Ensure dashboard exists
+    path("dashboard/", views.dashboard, name="dashboard"),  # Ensure dashboard exists
     path("add/", views.AddView.as_view(), name="add"),
     path("patron_list/", views.PatronView.as_view(), name="patron"),
     path("librarian_list/", views.LibrarianView.as_view(), name="librarian")
