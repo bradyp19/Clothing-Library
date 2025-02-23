e8a2da307097b9ff3a267fe9f6e2c218e69dbb3c
Zhiyuan Chang <vgs3qt@virginia.edu>
2025-02-23 03:15:58 -0500

    add url for dashboard

--- closet/urls.py
+++ closet/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.LoginView.as_view(), name="login"),
+    path("dashboard/", views.dashboard, name="dashboard"),  # Ensure dashboard exists
 ]
