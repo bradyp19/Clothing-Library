0119eafb9cbf1d5fd721d6bb3eb898c844794f47
Zhiyuan Chang <vgs3qt@virginia.edu>
2025-02-23 03:15:48 -0500

    created dashboard view for loged in user

--- closet/views.py
+++ closet/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.contrib.auth.decorators import login_required
 
 class LoginView(View):
     template_name = "closet/login.html"
+
+    def get(self, request, *args, **kwargs):  # Add GET request handling
+        return render(request, self.template_name)
+
+@login_required
+def dashboard(request):
+    return render(request, "closet/dashboard.html", {"user": request.user})
