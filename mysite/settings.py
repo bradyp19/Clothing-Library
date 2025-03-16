bc04c03f6f1ae82b30a9adb3ad6d3443975bcef6
graac0 <131699553+graac0@users.noreply.github.com>
2025-03-16 18:02:43 -0400

    Added media url to settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,6 +56,7 @@ AWS_S3_REGION_NAME = 'us-east-2'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_DEFAULT_ACL = 'public-read'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
