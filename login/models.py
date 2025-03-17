8313a7f6dd36b476b47fb79b3f3575f530d5094b
graac0 <131699553+graac0@users.noreply.github.com>
2025-03-16 20:20:30 -0400

    Created profile picture field in login model

--- login/models.py
+++ login/models.py
@@ -5,10 +5,12 @@ from django.contrib.auth.models import User
 
 class Patron(models.Model):
     patron = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='patron_profile_pictures/', null=True, blank=True)
     def __str__(self):
         return f'{self.user.email} Patron Profile'
 
 class Librarian(models.Model):
     librarian = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='librarian_profile_pictures/', null=True, blank=True)
     def __str__(self):
         return f'{self.user.email} Librarian Profile'
