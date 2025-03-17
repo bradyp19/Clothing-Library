60090301af1b85ff6b62fcf71e65ff6648a100cd
graac0 <131699553+graac0@users.noreply.github.com>
2025-03-16 23:43:41 -0400

    Fixed errors in settings and django.yml

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,7 +56,7 @@ AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_DEFAULT_ACL = 'public-read'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
