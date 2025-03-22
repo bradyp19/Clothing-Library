3311e96eb118c771f9859aca3931a99a96451119
Zhiyuan Chang <vgs3qt@virginia.edu>
2025-03-21 21:06:34 -0400

    two test for closet app

--- closet/tests.py
+++ closet/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class ClosetTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_closet_index_status_code(self):
+        response = self.client.get(reverse('closet:closet_index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_closet_index_template_used(self):
+        response = self.client.get(reverse('closet:closet_index'))
+        self.assertTemplateUsed(response, 'closet/closet_index.html')
--- closet/urls.py
+++ closet/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 app_name = "closet"
 urlpatterns = [
     path("", views.item_list, name="closet_index"),
